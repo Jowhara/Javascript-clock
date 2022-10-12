80dbcbce39afe5dd2f0d6269d9241ec8ec80f362
Jowhara <89601541+Jowhara@users.noreply.github.com>
2022-10-12 13:28:49 +0200

    updated .js & .css
    showTime, added details to different getHours and css positioning.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const showTime = function () {
     let date = new Date();
-    let h = date.getHours();
+    let h = date.getHours(); 
     let m = date.getMinutes();
     let s = date.getSeconds();
 
+    h = (h < 10 ? "0" + h : h); //adding 0 if less then 10
+    m = (m < 10 ? "0" + m : m);
+    s = (s < 10 ? "0" + s : s);
+
     let time = h + ":" + m + ":" + s;
+    
+    setTimeout(showTime, 1000); //updates time without reloading site.
     document.getElementById("MyClockDisplay").innerText = time;
 }
 
